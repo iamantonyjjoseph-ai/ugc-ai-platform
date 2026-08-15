81299e15530cb41ae8cdaa58560a069054e3c932
Antony joseph <iam.antonyjjoseph@gmail.com>
2026-08-15 08:16:50 +0530

    Update Buttons.tsx

--- client/src/components/Buttons.tsx
+++ client/src/components/Buttons.tsx
@@ -1,20 +1,12 @@
-import React from 'react'
+import React from "react";
 
-const { isSignedIn } = useUser();
-const navigate = useNavigate();
-
-export const PrimaryButton onClick={() => {
-    if (isSignedIn) {
-      navigate("/my-generations");
-    } else {
-      navigate("/generate");
-    }
-  }}
->
-  {isSignedIn ? "My Generations" : "Sign In"}: React.FC<React.ButtonHTMLAttributes<HTMLButtonElement>> = ({ children, className, ...props }) => (
-    <button className={`inline-flex items-center justify-center gap-2 rounded-full px-5 py-2 text-sm font-medium bg-linear-to-br from-indigo-500 to-indigo-600 hover:opacity-90 active:scale-95 transition-all ${className}`} {...props} >
-        {children}
-    </button>
+export const PrimaryButton: React.FC<
+  React.ButtonHTMLAttributes<HTMLButtonElement>
+> = ({ children, className, ...props }) => (
+  <button
+    className={`inline-flex items-center justify-center gap-2 rounded-full px-5 py-2 text-sm font-medium bg-linear-to-br from-indigo-500 to-indigo-600 hover:opacity-90 active:scale-95 transition-all ${className}`}
+    {...props}
+  >
+    {children}
+  </button>
 );
-
-
